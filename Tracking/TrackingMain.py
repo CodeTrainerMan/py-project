c92e06848090b96a39edc6af292dc5cc57b8d630
liule <451121814@qq.com>
2021-11-25 23:24:30 +0800

    handmouse

--- Tracking/TrackingMain.py
+++ Tracking/TrackingMain.py
@@ -1,38 +1,41 @@
 import cv2
 
-
-cap = cv2.VideoCapture("../2.mp4")
-# cap = cv2.VideoCapture(0)
-w
-#tracker = cv2.TrackerMOSSE_create()
+# cap = cv2.VideoCapture("../2.mp4")
+cap = cv2.VideoCapture(0)
+# tracker = cv2.TrackerMOSSE_create()
 tracker = cv2.TrackerCSRT_create()
+success, img = cap.read()
+bbox = cv2.selectROI("Tracking", img, False)
+tracker.init(img, bbox)
+
+
 def traking():
-
     success, img = cap.read()
-    bbox = cv2.selectROI("Tracking",img, False)
-    tracker.init(img,bbox)
-    return tracker,bbox,
+    bbox = cv2.selectROI("Tracking", img, False)
+    tracker.init(img, bbox)
+    return tracker, bbox,
 
-def drawBox(img,bbox):
-    x ,y, w, h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
-    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
-    cv2.putText(img,"Tracking",(75,75),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0))
+
+def drawBox(img, bbox):
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+    cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
+    cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0))
+
+
 while True:
 
-
-
     timer = cv2.getTickCount()
-    key = False;
+    key = False
     success, img = cap.read()
     if cv2.waitKey(1) & 0xff == ord("w"):
-        tracker,bbox=traking()
+        tracker, bbox = traking()
     key, bbox = tracker.update(img)
     if key:
-        drawBox(img,bbox);
+        drawBox(img, bbox)
     else:
         cv2.putText(img, "Lost", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
     cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.imshow("Tracking",img)
+    cv2.imshow("Tracking", img)
     if cv2.waitKey(1) & 0xff == ord("q"):
         break
